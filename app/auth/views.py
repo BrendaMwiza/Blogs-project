761e29f1fd0f61d25c2063ab2f1a13ca1ae75ee5
BrendaMwiza <brendabrizy@gmail.com>
2019-09-26 22:31:47 +0200

    added the views instances

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,52 @@
-from flask import Blueprint
+from flask import render_template,redirect,request,url_for,flash
+from flask_login import login_user,login_required,logout_user
+from . import auth
+from .. import db
+from ..models import Writer
+from .forms import RegistrationForm,LoginForm
 
-auth = Blueprint('auth',__name__)
 
-from . import views
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    '''
+    Function that checks if the form is validated
+    '''
+    if current_writer.is_authenticated:
+        return redirect(url_for('main.index'))
+
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        writer = Writer(email=login_form.email.data)
+        writer = Writer.query.filter_by(email=login_form.email.data).first()
+        if writer is not None and writer.verify_password(login_form.password.data):
+            login_user(writer,login_form.remember.data)
+            return redirect(request.args.get('next')or url_for('main.index'))
+
+        flash('invalid username or password')
+
+    return render_template('auth/login.html',login_form=login_form)
+
+
+@auth.route('/reqister',methods=['GET','POST'])
+def register():
+    '''
+    Registration function
+    '''
+    if current_writer.is_authenticated:
+        return redirect(url_for('main.index'))
+    form =RegistrationForm()
+    if form.validate_on_submit():
+        writer =Writer(email=form.email.data,name=form.username.data,password=form.password.data)
+        db.session.add(writer)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html',registration_form=form)
+
+#logout function
+@auth.route('/logout') 
+@login_required 
+def logout(): 
+    logout_user()
+    return redirect(url_for('main.index'))
--- app/main/views.py
+++ app/main/views.py
@@ -1,19 +1,18 @@
-from flask import render_template
-# ,request,redirect,url_for,flash,abort
+from flask import render_template,request,redirect,url_for,flash,abort
 from . import main
 from .. import db
-# from .forms import BlogForm, CommentForm, SubscriptionForm
+from .forms import BlogForm, CommentForm, SubscriptionForm
 from ..models import User,Writer,Blog,Comment,Subscription
 from flask_login import login_required,current_user
-# from app import login_manager
+from app import login_manager
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     '''
-#     @login_manager.user_loader Passes in a writer_id to this function
-#     Function queries the database and gets a user's id as a response
-#     '''
-#     return User.query.get(user_id)
+@login_manager.user_loader
+def load_user(user_id):
+    '''
+    @login_manager.user_loader Passes in a writer_id to this function
+    Function queries the database and gets a user's id as a response
+    '''
+    return User.query.get(user_id)
 
 #Views
 @main.route('/')
@@ -23,67 +22,67 @@ def index():
 
     return render_template('index.html', blogs=blogs)
 
-# @main.route('/blog', methods=['GET','BLOG'])
-# def new_blog():
-#     '''
-#     New Blog Page
-#     '''
-#     blog_form = BlogForm()
+@main.route('/blog', methods=['GET','POST'])
+def new_blog():
+    '''
+    New Blog Page
+    '''
+    blog_form = BlogForm()
 
-#     if blog_form.validate_on_submit():
-#         title = blog_form.title.data
-#         body = blog_form.body.data
-
-
-#         # updated review instance
-#         new_blog = Blog(title=title,body = body)
-
-#         #save review blog
-#         new_blog.save_blog()
-#         return redirect(url_for('main.index'))
-
-#     title = f'{blog.title}'
-#     return render_template('blog.html',title= title, blog_form=blog_form)
+    if blog_form.validate_on_submit():
+        title = blog_form.title.data
+        body = blog_form.body.data
 
 
-# @main.route('/comments/<int:id>', methods=['GET', 'BLOG'])
-# @login_required
-# def comments(id):
-#     comment_form = CommentForm()
-#     comment = Comment.query.order_by('-id').all()
+        # updated review instance
+        new_blog = Blog(title=title,body = body)
 
-#     if comment_form.validate_on_submit():
-#         comment = comment_form.comment.data()
-#         new_comment = Comment(comment=comment,users=current_user.username)
-#         new_comment.save_comment()
-#         return redirect(url_for('main.comments',id = blog.id))
-#     return render_template('comments.html')
+        #save review blog
+        new_blog.save_blog()
+        return redirect(url_for('main.index'))
 
-# @main.route('/delete/<int:id>')
-# @login_required
-# def delete(id):
-#     del_comment = Comment.query.get(id)
-#     db.session.delete(del_comment)
-#     db.session.commit()
-#     return redirect(url_for('main.index'))
-
-# @main.route('/subscription',methods = ["GET","BLOG"])
-# def subscriber():
-
-#     form= SubscriptionForm()
+    title = f'{blog.title}'
+    return render_template('blog.html',title= title, blog_form=blog_form)
 
 
-#     if form.validate_on_submit():
-#         email = form.email.data
-#         date = form.date.data
+@main.route('/comments/<int:id>', methods=['GET', 'POST'])
+@login_required
+def comments(id):
+    comment_form = CommentForm()
+    comment = Comment.query.order_by('-id').all()
+
+    if comment_form.validate_on_submit():
+        comment = comment_form.comment.data()
+        new_comment = Comment(comment=comment,users=current_user.username)
+        new_comment.save_comment()
+        return redirect(url_for('main.comments',id = blog.id))
+    return render_template('comments.html')
+
+@main.route('/delete/<int:id>')
+@login_required
+def delete(id):
+    del_comment = Comment.query.get(id)
+    db.session.delete(del_comment)
+    db.session.commit()
+    return redirect(url_for('main.index'))
+
+@main.route('/subscription',methods = ["GET","BLOG"])
+def subscriber():
+
+    form= SubscriptionForm()
 
 
-#         # updated review instance
-#         new_subscriber = Subscription(email=email,date = date,user_id=current_user.id)
-
-#         #save review method
-#         new_subscriber.save_subscriber()
-#         return redirect(url_for('subscriber'))
+    if form.validate_on_submit():
+        email = form.email.data
+        date = form.date.data
 
 
-#     return render_template('index.html',email= email, subscribe_form=form )
+        # updated review instance
+        new_subscriber = Subscription(email=email,date = date,user_id=current_user.id)
+
+        #save review method
+        new_subscriber.save_subscriber()
+        return redirect(url_for('subscriber'))
+
+
+    return render_template('index.html',email= email, subscribe_form=form )

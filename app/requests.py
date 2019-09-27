bb71d305f1b9311d5cf65bbd71a6cc15deff23d0
BrendaMwiza <brendabrizy@gmail.com>
2019-09-27 17:30:51 +0200

    added function to process quotes

--- app/requests.py
+++ app/requests.py
@@ -2,52 +2,39 @@ from app import app
 import urllib.request,json
 from .models import Quotes
 
-Movie = movie.Movie
 
-# Getting api key
-api_key = app.config['MOVIE_API_KEY']
+# Getting the base url
+base_url = app.config["QUOTES_API_BASE_URL"]
 
-# Getting the movie base url
-base_url = app.config["MOVIE_API_BASE_URL"]
-
-def get_movies(category):
+def get_quote(category):
     '''
     Function that gets the json response to our url request
     '''
-    get_movies_url = base_url.format(category,api_key)
+    get_quote_url = base_url.format(category,api_key)
 
-    with urllib.request.urlopen(get_movies_url) as url:
-        get_movies_data = url.read()
-        get_movies_response = json.loads(get_movies_data)
+    with urllib.request.urlopen(get_quote_url) as url:
+        get_quote_data = url.read()
+        get_quote_response = json.loads(get_quote_data)
 
-        movie_results = None
+        results_for = None
 
-        if get_movies_response['results']:
-            movie_results_list = get_movies_response['results']
-            movie_results = process_results(movie_results_list)
-    return movie_results
+        if get_quote_response['results']:
+            quotes_lists = get_quote_response['results']
+            results_for = process_results(quotes_lists)
+    return results_for
 
-def process_results(movie_list):
+def process_results(quote_list):
     '''
-    Function  that processes the movie result and transform them to a list of Objects
-
-    Args:
-        movie_list: A list of dictionaries that contain movie details
-
-    Returns :
-        movie_results: A list of movie objects
+    Function  that processes the quotes result and transform them to a list of Objects
     '''
-    movie_results = []
-    for movie_item in movie_list:
-        id = movie_item.get('id')
-        title = movie_item.get('original_title')
-        overview = movie_item.get('overview')
-        poster = movie_item.get('poster_path')
-        vote_average = movie_item.get('vote_average')
-        vote_count = movie_item.get('vote_count')
+    results_for = []
+    for quote_item in quote_list:
+        id = quote_item.get('id')
+        author = quote_item.get('original_author')
+        quote = quote_item.get('quote')
+        
+        if quote:
+            quote_object = Movie(id,author,quote)
+            results_for.append(quote_object)
 
-        if poster:
-            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
-            movie_results.append(movie_object)
-
-    return movie_results
+    return results_for
